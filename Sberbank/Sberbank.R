dc4705d11be1cb3d23d001b532bec2573a803eb5
Kshira Saagar <kshira.saagar@gmail.com>
2017-04-30 22:14:50 +1000

    Trying a GLM

--- Sberbank/Sberbank.R
+++ Sberbank/Sberbank.R
@@ -10,31 +10,30 @@ library(party)
 library(readr)
 library(corrplot)
 
-test <- read.csv('test.csv', stringsAsFactors = TRUE)
 train <- read.csv('train.csv', stringsAsFactors = TRUE)
+test <- read.csv('test.csv', stringsAsFactors = TRUE)
 macro <- read.csv('macro.csv', stringsAsFactors = TRUE)
 
 train <- merge(train, macro, by = 'timestamp')
+test <- merge(test, macro, by = 'timestamp')
 
 fit <- rpart(price_doc ~ .,
              data = train[-c(1,2,13)],
-             control = rpart.control(cp = .05))
+             control = rpart.control(cp = .001))
 
-attr(fit$terms,"term.labels")
+variables <- data.frame(fit$variable.importance)
+variables$variable <- row.names(variables)
+row.names(variables) <- NULL
+colnames(variables) <- c('importance','variable')
 
-# train_corr <- train %>% 
-#               select(price_doc,
-#                      cpi,
-#                      ppi,
-#                      eurrub,
-#                      usdrub,
-#                      mortgage_value,
-#                      mortgage_growth,
-#                      mortgage_rate,
-#                      salary,
-#                      employment,
-#                      marriages_per_1000_cap,
-#                      construction_value) %>% 
-#               cor(use = "complete.obs")
-# 
-# corrplot.mixed(train_corr)
+train_subset <- train %>% select(price_doc, one_of(variables$variable))
+
+glm_fit <- glm(price_doc ~ .,
+               family = 'poisson',
+               data = train_subset)
+
+prediction <- predict(glm_fit, test)
+
+submit <- data.frame(id = test$id,
+                     price_doc = prediction)
+
