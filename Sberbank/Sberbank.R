c8a768a2218651a46ae5978bc3983e3b5f5ca97b
Kshira Saagar <kshira.saagar@gmail.com>
2017-05-01 23:04:07 +1000

    Minor progress

--- Sberbank/Sberbank.R
+++ Sberbank/Sberbank.R
@@ -9,6 +9,9 @@ library(rpart.plot)
 library(party)
 library(readr)
 library(corrplot)
+library(Boruta)
+library(caret)
+library(pROC)
 
 train <- read.csv('train.csv', stringsAsFactors = TRUE)
 test <- read.csv('test.csv', stringsAsFactors = TRUE)
@@ -17,9 +20,29 @@ macro <- read.csv('macro.csv', stringsAsFactors = TRUE)
 train <- merge(train, macro, by = 'timestamp')
 test <- merge(test, macro, by = 'timestamp')
 
+summary(train$price_doc)
+summary(train$full_sq)
+summary(train$price_doc/train$full_sq)
+
+train$dataset <- 'train'
+test$dataset <- 'test'
+
+#Just guessing based on logical distributions
+test$price_doc <- test$full_sq * 135300 + (rnorm(nrow(test), mean = 6274, sd = 4740))
+
+#Overall dataset for easier missing value and outlier treatment
+overall <- rbind(train, test)
+
+#Feature Importance - Attempt 2
+features <- overall %>% select(-timestamp,-id, -price_doc, -dataset) %>% names()
+
+
+
+#Feature Importance - Attempt 1
+#CART to identify top 100 features
 fit <- rpart(price_doc ~ .,
              data = train[-c(1,2,13)],
-             control = rpart.control(cp = .001))
+             control = rpart.control(cp = .05))
 
 variables <- data.frame(fit$variable.importance)
 variables$variable <- row.names(variables)
@@ -37,3 +60,4 @@ prediction <- predict(glm_fit, test)
 submit <- data.frame(id = test$id,
                      price_doc = prediction)
 
+write.csv(submit,paste0('submit',Sys.Date(),'.csv'),row.names = FALSE)
