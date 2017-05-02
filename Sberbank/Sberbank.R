f7446196b874b70e68ccea963e0b9521bfe24cef
Kshira Saagar <kshira.saagar@gmail.com>
2017-05-02 22:37:48 +1000

    0.3498 to 0.3358

--- Sberbank/Sberbank.R
+++ Sberbank/Sberbank.R
@@ -27,26 +27,37 @@ summary(train$price_doc/train$full_sq)
 train$dataset <- 'train'
 test$dataset <- 'test'
 
-#Just guessing based on logical distributions
+#GUESS 1 - Just guessing based on logical distributions
 test$price_doc <- test$full_sq * 135300 + (rnorm(nrow(test), mean = 6274, sd = 4740))
 
-#Now find the price by area
-train$state[is.na(train$state)] <- 0
+###################   MISSING VALUES  ###############################
 
+#Overall dataset for easier missing value and outlier treatment
+overall <- rbind(train, test)
+
+#Missing Value Treatment
+overall$state[is.na(overall$state)] <- 0
+
+overall$build_year[is.na(overall$build_year)] <- 2020
+overall$build_year[overall$build_year < 1600] <- 2017 - overall$build_year[overall$build_year < 1600]
+overall$build_year[overall$build_year > 2020] <- 2009
+
+train <- overall %>% filter(dataset == "train")
+test <- overall %>% filter(dataset == "test")
+
+####################################################################
+
+#GUESS 2 - Estimated based on key variables numerically
 price_by_area <- train %>% 
                        group_by(state, sub_area, product_type) %>% 
                        summarise(med_price = median(price_doc, na.rm = TRUE)/median(full_sq, na.rm = TRUE))
 
 test2 <- merge(test, price_by_area, by = c('state','sub_area','product_type'), all.x = TRUE)
+sum(is.na(test2$med_price))
+
 test2$med_price[is.na(test2$med_price)] <- 135300
 
 test2$price_doc <- test2$full_sq * test2$med_price + (rnorm(nrow(test), mean = 6274, sd = 4740))
-
-#Overall dataset for easier missing value and outlier treatment
-overall <- rbind(train, test)
-
-#Feature Importance - Attempt 2
-features <- overall %>% select(-timestamp,-id, -price_doc, -dataset) %>% names()
 
 #Feature Importance - Attempt 1
 #CART to identify top 100 features
@@ -67,6 +78,9 @@ glm_fit <- glm(price_doc ~ .,
 
 prediction <- predict(glm_fit, test)
 
+#Feature Importance - Attempt 2
+features <- overall %>% select(-timestamp,-id, -price_doc, -dataset) %>% names()
+
 
 #Submission Codes
 
@@ -75,4 +89,4 @@ submit <- data.frame(id = test$id,
 
 submit <- test2 %>% select(id, price_doc)
 
-write.csv(submit,paste0('submit',Sys.Date(),'.csv'),row.names = FALSE)
+write.csv(submit,paste0('submit2',Sys.Date(),'.csv'),row.names = FALSE)
